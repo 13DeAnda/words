f96e0fedd0a3889e7bbda03f0b8b065499e46dc4
13DeAnda <nora.e.de.anda@gmail.com>
2022-06-25 13:41:31 -0500

    basic retrivel of words from api

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -2,9 +2,26 @@ import React, { useState, useEffect } from 'react';
 import './Container.css';
 
 import Word from '../Word/Word';
+import axios from 'axios';
+
+const baseURL = 'https://random-word-api.herokuapp.com/word';
 
 export default function Container() {
-    const [word, setWord] = useState<string>('authoritharian');
+    const [word, setWord] = useState<string>('');
+    const [post, setPost] = React.useState(null);
+
+    const retriveWord = () => {
+        axios.get(baseURL).then((response) => {
+            setPost(response.data);
+            setWord(response.data[0]);
+        });
+    };
+
+    useEffect(() => {
+        retriveWord();
+    }, []);
+
+    if (!post) return null;
 
     return (
         <div className="Container">
