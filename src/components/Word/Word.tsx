b5ac29919973fd894a6422e157451ae31e0914e7
13DeAnda <nora.e.de.anda@gmail.com>
2022-06-26 17:43:21 -0500

    word displaying

--- src/components/Word/Word.tsx
+++ src/components/Word/Word.tsx
@@ -1,21 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import './Word.css';
 
+interface wordMapType {
+    letter: string;
+    found: boolean;
+}
+
 function Word(props: { word: string }) {
     const [wordArray, setWordArray] = useState(['']);
+    const [typedWord, setTypedWord] = useState<wordMapType[]>([]);
 
     useEffect(() => {
         if (wordArray.length === 1 && props.word.length) {
             setWordArray(props.word.split(''));
+            //pushing blank word
+            const tempArr = [];
+            for (let i = 0; i < props.word.length; i++) {
+                tempArr.push({ letter: props.word[i], found: false });
+            }
+            setTypedWord(tempArr);
         }
     });
 
     return (
         <div className="Word">
-            {wordArray.map((letter: string, index: number) => (
+            {typedWord.map((space: wordMapType, index: number) => (
                 <div className="letterContainer" key={`letter${index}`}>
                     {' '}
-                    {letter.toLocaleUpperCase()}
+                    {space.found ? space.letter.toLocaleUpperCase() : '.'}
                 </div>
             ))}
         </div>
