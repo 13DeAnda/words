214207a4c32f29162155ebd1de7fd4f289c660ba
13DeAnda <nora.e.de.anda@gmail.com>
2022-07-27 15:12:57 -0500

    stats page frontend

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,26 +1,60 @@
 import React, { useState, useEffect } from 'react';
 import './users.css';
 import { getTopUsers, loadingTopUsers } from '../../services/TopScoresService';
+import { getUser } from '../../services/GameService';
 import { TopUserI } from '../../Interfaces/topUser';
+import { UserI } from '../../Interfaces/user';
 
+//TODO: temp this should be cached at login or score recording
+// TODO: must introduce loading icons
+const userId = 1;
 function Users() {
     const [topScores, setTopScores] = useState<TopUserI[]>([]);
+    const [user, setUser] = useState<UserI | null>(null);
 
-    const retriveTopUsers = async () => {
-        const users = getTopUsers();
-        console.log('the top users', users);
+    const retriveTopScores = async () => {
+        const users = await getTopUsers();
+        setTopScores(users);
+    };
+    const retriveUser = async () => {
+        const user = await getUser(userId);
+        setUser(user);
     };
 
     useEffect(() => {
-        if (!topScores && !loadingTopUsers) {
-            retriveTopUsers;
+        if (!loadingTopUsers) {
+            retriveTopScores();
+            retriveUser();
         }
     }, []);
 
     return (
         <div className="Container">
-            <div className="topScores">Top Scores</div>
-            <div>Your Stats</div>
+            {topScores && (
+                <div className="topScores">
+                    <h3>Top High Scores</h3>
+                    {topScores.map((user, index) => (
+                        <div
+                            key={`topUser_${index}`}
+                            className={user.userId == userId ? 'yourTopScore topScore' : 'topScore'}
+                        >
+                            <div>
+                                <b># {user.rank}</b> {user.username}{' '}
+                            </div>
+                            <div>
+                                <b> {user.score} pts</b>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            )}
+
+            {user && (
+                <div className="topScores">
+                    <h3>Your Score</h3>
+                    <div className="userScore">{user?.score} pts</div>
+                </div>
+            )}
         </div>
     );
 }
